6cb08241b836ef2043cbec79e63091cd4d5cdff6
grupoubaranas <grupoubaranas@gmail.com>
2025-11-09 16:34:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,10 +7,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     const ctaButtons = document.querySelectorAll('.cta-button');
     
-    ctaButtons.forEach(button => {
+    ctaButtons.forEach( button => {
         button.addEventListener('click', () => {
             // Rastreamento de um evento de Lead customizado no clique do CTA
-            if (typeof fbq === 'function') {
+            if ( typeof fbq === 'function') {
                 fbq('track', 'Lead', {
                     content_name: 'Manutencao_Eletrica_WhatsApp',
                     value: 1.00, // Valor simbólico de 1.00 para o Lead
@@ -21,3 +21,4 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
