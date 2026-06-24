8a8be0e06024b2b17f31b7eaaf270a104a7415aa
grupoubaranas <grupoubaranas@gmail.com>
2026-06-24 14:18:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -182,3 +182,21 @@ console.log(
 "Grupo Ubaranas carregado com sucesso"
 
 );
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.remove("active");
+    if (i === index) {
+      slide.classList.add("active");
+    }
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 5000);
